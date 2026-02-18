94ba82ccbb7e7a907961c3bfeb5f94a9d9db66bd
Serg97977 <sergeynorekyan6@gmail.com>
2026-02-19 01:38:34 +0400

    There was a bug about creating account

--- app/services/user_functions.py
+++ app/services/user_functions.py
@@ -67,7 +67,10 @@ def show_transaction_history():
             mycursor.execute(transaction_history_query, (user_id,))
 
             transaction_history = mycursor.fetchall()
-            mycursor.close()
+            if not transaction_history:
+                raise UserError
+            else:
+                mycursor.close()
 
             # Note to dev: Fetches only the first entry; logic for multiple entries needed
             return (f"From your card :{transaction_history[0][0]}\n"
